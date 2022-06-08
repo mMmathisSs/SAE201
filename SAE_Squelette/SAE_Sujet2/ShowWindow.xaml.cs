32eda93f761e21c555c9d8bb87cbe3c14d4a3adc
chaussoc <chaussoc@D353-S03>
2022-06-08 09:53:31 +0200

    ouioui

--- SAE_Squelette/SAE_Sujet2/ShowWindow.xaml.cs
+++ SAE_Squelette/SAE_Sujet2/ShowWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SAE_Squelette;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,9 +19,22 @@ namespace SAE_Sujet2
     /// </summary>
     public partial class ShowWindow : Window
     {
+        public ObservableCollection<Mission> LesMissions { get; set; }
         public ShowWindow()
         {
             InitializeComponent();
+            LesMissions = new ObservableCollection<Mission>();
+            LesMissions.Add(new Mission(1, "Mission 1", new DateTime(01,01,01)));
+            LesMissions.Add(new Mission(2, "Mission 2", new DateTime(01, 01, 01)));
+            LesMissions.Add(new Mission(3, "Mission 3", new DateTime(01, 01, 01)));
+            LesMissions.Add(new Mission(4, "Mission 4", new DateTime(01, 01, 01)));
+            LesMissions.Add(new Mission(5, "Mission 5", new DateTime(01, 01, 01)));
+            this.DataContext = this;
+        }
+
+        private void butRetour_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
