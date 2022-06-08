23580d0d7ba21e27817202913301a54c3bbe4117
Yanis Bellahcene <yanis.bellahcene@etu.univ-smb.fr>
2022-06-08 09:12:29 +0200

    Corige bug

---
+++ SAE_Squelette/SAE_Sujet2/AddAffectWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SAE_Sujet2
+{
+    /// <summary>
+    /// Logique d'interaction pour AddAffectWindow.xaml
+    /// </summary>
+    public partial class AddAffectWindow : Window
+    {
+        public AddAffectWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
