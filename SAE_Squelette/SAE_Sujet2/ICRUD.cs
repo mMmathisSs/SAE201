f7f92652911be5e0679998464c4be32248350f58
claviozm <claviozm@D353-S02>
2022-06-15 11:19:50 +0200

    commentaire suite

--- SAE_Squelette/SAE_Sujet2/ICRUD.cs
+++ SAE_Squelette/SAE_Sujet2/ICRUD.cs
@@ -3,13 +3,43 @@ using System.Collections.Generic;
 
 namespace SAE_Squelette
 {
+	/// <summary>
+	/// Permet d'implémenter le CRUD dans les différentes classes
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
 	public interface ICRUD<T>
 	{
+		/// <summary>
+		/// Méthode pour ajouter un  élément
+		/// </summary>
 		void Create();
+
+		/// <summary>
+		/// Méthode pour lire un élément
+		/// </summary>
 		void Read();
+
+		/// <summary>
+		/// Méthode pour mettre à jour un élément
+		/// </summary>
 		void Update();
+
+		/// <summary>
+		/// Méthode pour supprimer un élément
+		/// </summary>
 		void Delete();
+
+		/// <summary>
+		/// Méthode pour extraire les éléments de la BD
+		/// </summary>
+		/// <returns></returns>
 		List<T> FindAll();
+
+		/// <summary>
+		/// Méthode pour extraire les éléments de la BD avec un filtre
+		/// </summary>
+		/// <param name="criteres"></param>
+		/// <returns></returns>
 		List<T> FindBySelection(string criteres);
 
 	}
