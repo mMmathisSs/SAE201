d0693f8487fddafe7b633cdc17db0eba6d1a3836
mMmathisSs <mathis.clavioz@etu.univ-smb.fr>
2022-06-19 19:56:01 +0200

    aa

--- SAE_Squelette/SQLQuery1.sql
+++ SAE_Squelette/SQLQuery1.sql
@@ -1,4 +1,10 @@
-﻿delete from EFFECTUE2 where COMMENTAIRE = 'test update 4444';
+﻿delete from EFFECTUE2 where COMMENTAIRE = 'test find 4444';
+delete from mission where LIBELLEMISSION = 'test find 4444';
+delete from EFFECTUE2 where COMMENTAIRE = 'test delete 4444';
+delete from mission where LIBELLEMISSION = 'test delete 4444';
+delete from EFFECTUE2 where COMMENTAIRE = 'test update 4444';
 delete from mission where LIBELLEMISSION = 'test update 4444';
+delete from EFFECTUE2 where COMMENTAIRE = 'test update 8888';
+delete from mission where LIBELLEMISSION = 'test update 8888';
 
 select * from EFFECTUE2;
